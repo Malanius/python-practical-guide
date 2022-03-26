0a3aceb65dbd066bc5342fee8ad568f42c6eb213
Malanius Privierre <malaniusprivierre@gmail.com>
2022-03-26 12:09:59 +0100

    📝 (functions): Add docstrings to blockaing functions

--- src/python_practical_guide/blockchain.py
+++ src/python_practical_guide/blockchain.py
@@ -2,14 +2,22 @@ blockchain = []
 
 
 def get_last_block_value():
+    """Returns  the last block value of the current blockchain."""
     return blockchain[-1]
 
 
 def add_value(transaction_amount, last_transaction=[1]):
+    """Appends a new value as well as the last block value to the blockchain.
+
+    Arguments:
+        :transaction_amount: The amoint that should be added.
+        :last_transaction: The last blockchain transaction (default [1]).
+    """
     blockchain.append([last_transaction, transaction_amount])
 
 
 def get_user_input():
+    """Returns the transaction amount from user input."""
     return float(input('Enter transaction amount: '))
 
 
