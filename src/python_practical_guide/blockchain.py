d039803ea628273c4aaf4019827ae5d92e06407f
Malanius Privierre <malaniusprivierre@gmail.com>
2022-03-26 14:41:11 +0100

    ✨ (conditionals): Ask which action user wants to do

--- src/python_practical_guide/blockchain.py
+++ src/python_practical_guide/blockchain.py
@@ -16,20 +16,39 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     """Returns the transaction amount from user input."""
     return float(input('Enter transaction amount: '))
 
 
-tx_amount = get_user_input()
-add_value(tx_amount)
+def get_user_choice():
+    return input('Your choice: ')
 
-while True:
-    tx_amount = get_user_input()
-    add_value(tx_amount, get_last_block_value())
 
+def print_blocks():
     print('Blocks in chain:')
     for block in blockchain:
         print(block)
+
+
+tx_amount = get_transaction_value()
+add_value(tx_amount)
+
+while True:
+    print('Choose operation:')
+    print('1: Add new transaction value')
+    print('2: Print the blocks')
+    print('3: Exit')
+    user_choice = get_user_choice()
+
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_block_value())
+    elif user_choice == '2':
+        print_blocks()
+    elif user_choice == '3':
+        break
+    else:
+        print('Invalid choice!')
 
 print('Done!')
