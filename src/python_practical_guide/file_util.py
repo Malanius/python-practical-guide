796c2c8a7fd127dce02c5a4c8e0f94fe52709a1d
Malanius Privierre <malaniusprivierre@gmail.com>
2022-04-06 18:36:54 +0200

    🐛 (blockchain): Load saved transactions data to OrderedDict

--- src/python_practical_guide/file_util.py
+++ src/python_practical_guide/file_util.py
@@ -1,4 +1,5 @@
 import json
+from collections import OrderedDict
 
 DATA_FILE = 'blockchain.json'
 
@@ -11,8 +12,20 @@ def save_data(blockchain, open_transactions):
         }))
 
 
+def convert_transactions(transactions):
+    return [OrderedDict([('sender', tx['sender']), ('recepient', tx['recepient']), ('amount', tx['amount'])]) for tx in transactions]
+
+
 def load_data():
-    data = {}
     with open(DATA_FILE, mode='r') as file:
-        data = json.loads(file.read())
-    return data
+        file_data = json.loads(file.read())
+    blockchain = [{'previous_hash': block['previous_hash'],
+                   'index': block['index'],
+                   'proof': block['proof'],
+                   'transactions': convert_transactions(block['transactions'])
+                   } for block in file_data['blockchain']]
+    open_transactions = convert_transactions(file_data['open_transactions'])
+    return {
+        'blockchain': blockchain,
+        'open_transactions': open_transactions
+    }
