c83fb42780fbdfb053e320eeffd3d967dcc8311f
Malanius Privierre <malaniusprivierre@gmail.com>
2022-04-16 10:16:53 +0200

    ✨ (http): Create endpoint to get current chain

--- src/core/node.py
+++ src/core/node.py
@@ -1,18 +1,26 @@
-from crypt import methods
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 from core.wallet import Wallet
+from core.blockchain import Blockchain
 
 app = Flask(__name__)
 CORS(app)
 
 wallet = Wallet()
+blockchain = Blockchain(wallet.public_key)
 
 
 @app.route('/', methods=['GET'])
 def get_ui():
     return 'Works!'
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain = blockchain.chain
+    dumpable_chain = [block.convert_to_dumpable_block() for block in chain]
+    return jsonify(dumpable_chain), 200
 
 
 if __name__ == '__main__':
