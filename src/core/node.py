211e1e1128bee32f521bd5956c81dd47edd4aa0e
Malanius Privierre <malaniusprivierre@gmail.com>
2022-04-16 10:44:39 +0200

    ✨ (http): Add endpoint to mine blocks

--- src/core/node.py
+++ src/core/node.py
@@ -23,5 +23,22 @@ def get_chain():
     return jsonify(dumpable_chain), 200
 
 
+@app.route('/mine', methods=['POST'])
+def mine_block():
+    block = blockchain.mine_block()
+    if block != None:
+        response = {
+            'message': 'Block added sucesfully.',
+            'block': block.convert_to_dumpable_block()
+        }
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': 'Adding block failed!',
+            'wallet_set_up': wallet.public_key != None
+        }
+        return jsonify(response), 400
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
